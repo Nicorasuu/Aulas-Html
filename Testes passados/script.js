778f4789b6e68ca72e1b84ce8e7ea540c45c0358
Nicorasuu <nicolaswo96@gmail.com>
2022-10-08 18:57:31 -0300

    aulas 6-9

---
+++ Testes passados/script.js
@@ -0,0 +1,13 @@
+let nome = "Bonieky" // string
+let idade = 45.5 // number
+let cidade = "Tokyo" // string
+let country = "Japan" // string
+let logado = true // boolean  
+let lista = ["Ovo", "Leite", "Massa", "Corante"] // array
+let nomeCompleto = {nome: "Bonieky", idade: 45.5 } //object
+
+
+/* o comando abaixo exibe os dados no Console  
+*/
+
+console.log (nome)
