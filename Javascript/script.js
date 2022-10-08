44846f45ccac11549ef61aa1773f741d9f663ab2
Nicorasuu <nicolaswo96@gmail.com>
2022-10-08 17:32:25 -0300

    Aulas 3-6

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,1 +1,17 @@
-console.log("Olá Mundo 3.0")
+let nome = "Bonieky"
+let idade = 45.5
+let cidade = "Tokyo"
+let country = "Japan"
+let logado = true
+let lista = ["Ovo", "Leite", "Massa", "Corante"]
+let nomeCompleto = {nome: "Bonieky", idade: 45.5 }
+
+
+
+console.log (typeof nome)
+console.log (typeof idade)
+console.log (cidade)
+console.log (country)
+console.log (typeof lista)
+console.log (typeof logado)
+console.log (typeof nomeCompleto)
