e1a5d8cd04f1c2f3d42bb728e968e869e0e84e94
Nicorasuu <nicolaswo96@gmail.com>
2022-10-11 12:14:51 -0300

    11/10

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,17 +1,11 @@
-let nome = "Bonieky"
-let idade = 45.5
-let cidade = "Tokyo"
-let country = "Japan"
-let logado = true
-let lista = ["Ovo", "Leite", "Massa", "Corante"]
-let nomeCompleto = {nome: "Bonieky", idade: 45.5 }
+let nomedocarro = "Ferrari"; 
+let preçodobolo = "R$ 19,99"; 
+let cidade = "São Paulo";
 
+console.log (nomedocarro);
+console.log (preçodobolo);
+console.log (cidade);
 
-
-console.log (typeof nome)
-console.log (typeof idade)
-console.log (cidade)
-console.log (country)
-console.log (typeof lista)
-console.log (typeof logado)
-console.log (typeof nomeCompleto)
+alert (nomedocarro);
+alert (preçodobolo);
+alert (cidade);
