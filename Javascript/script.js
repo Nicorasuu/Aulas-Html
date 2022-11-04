2cf03a0707622547a2c6e9a320a1b9547ba81238
Nicorasuu <nicolaswo96@gmail.com>
2022-11-04 19:55:40 -0300

    04/11 Hj n rendeu nada

---
+++ Javascript/script.js
@@ -0,0 +1,9 @@
+//REVISANDO DOM:
+
+//Alterando um elemento do HTML da pagina:
+
+//document.getElementById("exemplo").innerHTML = "Pedro <button>Button</button>"
+
+//Alterando os elementos pela Classe: 
+//document.getElementsByClassName("lista")[0].innerHTML = 'Arroz, Feijao, Carne'
+
